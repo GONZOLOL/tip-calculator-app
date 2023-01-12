412c1068e7a8d9bf44c2f0b1c05fa4077d3257b0
GONZOLOL <glerdie489@g.educaand.es>
2023-01-12 10:00:26 +0100

    Cambios dia 11/01 en relacion con la logica

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-var bill = document.getElementById("bill").value;
-var people = document.getElementById("people").value;
+let bill = document.getElementById("bill").value;
+let people = document.getElementById("people").value;
+let tip = 0;
 
-if ( document.getElementsByName('tip').value ) {
-    var radios = document.getElementsByName('tip');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            var tip = radio.value;
-        }
-    }   
-}
-else if (document.getElementById("custom").value) {
-    var tip = document.getElementById("custom").value;
+function btn_value(val){
+    tip  = val;
+    console.log(tip);
 }
 
+function getCustomValue(val) {
+    tip = val;
+    console.log(tip);
+}
+
+let tip_amount = ((tip*bill)/100) / people
+let total = tip_amount + bill /2;
+
+function run() {
+    document.getElementById("tip-amount").innerHTML ="$" + tip_amount.toFixed(2);
+    document.getElementById("total").innerHTML ="$" + total.toFixed(2);
+    console.log("esto funciona");
+}
 
 document.getElementById('custom').onclick = function() {
     var checkboxes = document.getElementsByName('tip');
     for (var checkbox of checkboxes) {
         checkbox.checked = false;
     }
-}
-
-var tip_amount = ((tip*bill)/100) / people
-var total = tip_amount + bill /2;
-
-if (bill && people && tip) {
-    document.getElementById("tip-amount").innerHTML ="$" + tip_amount.toFixed(2);
-    document.getElementById("total").innerHTML ="$" + total.toFixed(2);
 }
