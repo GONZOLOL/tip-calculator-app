143d89b03a83e0b6ec79fdf7920fc016f4b653b4
GONZOLOL <glerdie489@g.educaand.es>
2023-01-13 14:32:58 +0100

    Deteccion de valores incorrectos, logica y visual

--- main.js
+++ main.js
@@ -22,13 +22,15 @@ function getCustomValue(val) {
 
 
 function run() {
-    tip_amount = ((tip*bill)/100) / people
-    total = tip_amount + bill /2;
-
-    document.getElementById("tip-amount").innerHTML ="$" + tip_amount.toFixed(2);
-    document.getElementById("total").innerHTML ="$" + total.toFixed(2);
-
-    console.log("esto funciona");
+    if (bill && tip && people && bill > 0 && people > 0 && tip > 0 && tip <= 100){
+        tip_amount = ((tip*bill)/100) / people
+        total = tip_amount + bill /2;
+    
+        document.getElementById("tip-amount").innerHTML ="$" + tip_amount.toFixed(2);
+        document.getElementById("total").innerHTML ="$" + total.toFixed(2);
+    
+        console.log("esto funciona");
+    }
 }
 
 document.getElementById('custom').onclick = function() {
@@ -37,3 +39,21 @@ document.getElementById('custom').onclick = function() {
         checkbox.checked = false;
     }
 }
+
+//Detectar Valores incorrectos
+let test = document.querySelector('.guess');
+test.addEventListener('keypress', function (event) {
+    const teclaKey = event.key;
+    const teclaCode = event.code;
+
+    if (teclaKey === '-' || teclaCode === 'Slash') {
+    console.log('La cagó');
+    console.warn('Valor Incorrecto');
+    this.style.borderColor = '#FF0000';
+    } else if (teclaKey === 'Backspace' || teclaCode === 'Backspace') {
+    console.log('Ya la arregló');
+    } else {
+    console.log(teclaKey, teclaCode);
+    this.style.borderColor = '#FFFFFF';
+    }
+});
